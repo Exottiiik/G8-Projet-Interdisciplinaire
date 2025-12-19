4f3bd8caee3f5dc749daced42d538ad4d0f54ad9
h3rmanos <daniel.herman@std.heh.be>
2025-12-19 12:06:51 +0100

    FEAT :  listing des fichiers, suppression,..

--- intranet/documents.php
+++ intranet/documents.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php');
+    exit;
+}
+
 $pageTitle = "Documents Internes";
 $themeClass = "theme-intranet";
 
